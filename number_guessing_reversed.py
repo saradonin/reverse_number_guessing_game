bafdf469ff32d2716efdc5eb1b39f86c6cf296b2
saradonin <m.jemieljanczuk@gmail.com>
2023-03-28 23:58:46 +0200

    Temp variables removed

--- number_guessing_reversed.py
+++ number_guessing_reversed.py
@@ -1,6 +1,6 @@
 def guess(min=1, max=1000):
     """
-     Play a game where the computer guesses a number in given range (between 1 and 1000 by default) in 10 tries or less.
+     Play a game where the computer guesses a number in given range (between 1 and 1000 by default) in 10 tries or fewer
      Answer the questions with 'Y' for yes and 'N' for no.
      Returns 'I won!' if the computer guesses the number correctly, or an error message if the user cheats.
     :param min: int - the smallest number in range
@@ -8,18 +8,20 @@ def guess(min=1, max=1000):
     :return: str
     """
 
-    print("""Let's play a game. Think of a number between 1 and 1000 and I'll guess it in 10 tries or less. Answer my questions by pressing keys on your keyboard: 
+    print("""Let's play a game. Think of a number between 1 and 1000 and I'll guess it in 10 tries or fewer. 
+Answer my questions by pressing keys on your keyboard: 
 'Y' for Yes or 'N' for No. 
   """)
     count = 0
     while True:
         guess = int((max - min) / 2 + min)
 
-        print(f"My guess is: {guess} ({count}.try)")
+        print(f"My guess is: {guess}")
         print("Did I guess? [y/n]")
+        count += 1
 
         answer_1 = input().lower()
-        count += 1  # tries counter
+
         if answer_1 == 'y':
             return "I won!"
         elif answer_1 == 'n':
