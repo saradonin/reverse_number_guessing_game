2d5bdfd0abd148805cd3dae4ee5e83f88574b281
saradonin <m.jemieljanczuk@gmail.com>
2023-03-29 16:32:02 +0200

    User answer validity tests refactored for visibility

--- number_guessing_reversed.py
+++ number_guessing_reversed.py
@@ -8,8 +8,9 @@ def guess(min=1, max=1000):
     :return: str
     """
 
-    print("""Let's play a game. Think of a number between 1 and 1000 and I'll guess it in 10 tries or fewer. 
-Answer my questions by pressing keys on your keyboard: 
+    print("""Let's play a game. 
+Think of a number between 1 and 1000 and I'll guess it in 10 tries or fewer. 
+Answer my questions by pressing keys: 
 'Y' for Yes or 'N' for No. 
   """)
     count = 0
@@ -21,29 +22,31 @@ Answer my questions by pressing keys on your keyboard:
         count += 1
 
         answer_1 = input().lower()
-
-        if answer_1 == 'y':
+        if answer_1 not in ('y', 'n'):
+            print("Please answer with: 'y' for Yes or 'n' for No")
+            continue
+        elif answer_1 == 'y':
             return "I won!"
         elif answer_1 == 'n':
             print("Too big? [y/n]")
             answer_2 = input().lower()
 
-            if answer_2 == 'y':
+            if answer_2 not in ('y', 'n'):
+                print("Please answer with: 'y' for Yes or 'n' for No")
+                continue
+            elif answer_2 == 'y':
                 max = guess
             elif answer_2 == 'n':
                 print("Too small? [y/n]")
                 answer_3 = input().lower()
 
-                if answer_3 == 'y':
+                if answer_3 not in ('y', 'n'):
+                    print("Please answer with: 'y' for Yes or 'n' for No")
+                    continue
+                elif answer_3 == 'y':
                     min = guess
                 elif answer_3 == 'n':
                     print("Don't cheat!")
-                else:
-                    print("Please answer with: 'y' for Yes or 'n' for No")
-            else:
-                print("Please answer with: 'y' for Yes or 'n' for No")
-        else:
-            print("Please answer with: 'y' for Yes or 'n' for No")
 
 
 print(guess())
